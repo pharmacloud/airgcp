89841b701cf07a03091bf7a8cff60866803528bf
Yorimoto Komori <komori@miraitechno.com>
2023-11-20 13:09:05 +0900

    コードの整理

--- environment/env.go
+++ environment/env.go
@@ -2,9 +2,8 @@ package environment
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"os"
+	"strings"
 
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
@@ -20,26 +19,37 @@ type Config struct {
 var defaultEnvfile = ".air-env.toml"
 
 func InitEnv(ctx context.Context, path string) error {
+	// パスが指定していない場合にはディフォルトパスを指定
 	if path == "" {
 		path = defaultEnvfile
 	}
+	// 設定の読み込み
 	cfg, err := readConfig(path)
 	if err != nil {
 		return nil
 	}
-	log.Printf("???: %+v", cfg)
+	// ProjectIdが指定していない場合は何もしない
+	if cfg.ProjectId == "" {
+		return nil
+	}
+
+	// 環境変数の読み込み
+	env := readEnv(cfg)
+	// シークレットの読み込み
 	sec, err := readSecret(ctx, cfg)
 	if err != nil {
 		return err
 	}
 
+	// ProjectIdを既定の環境変数として設定
 	if err := os.Setenv("GOOGLE_CLOUD_PROJECT", cfg.ProjectId); err != nil {
 		return err
 	}
-
-	if err := setEnv(cfg.Env); err != nil {
+	// 環境変数の設定
+	if err := setEnv(env); err != nil {
 		return err
 	}
+	// シークレットを環境変数として指定
 	if err := setEnv(sec); err != nil {
 		return err
 	}
@@ -60,6 +70,14 @@ func readConfig(path string) (*Config, error) {
 	return cfg, nil
 }
 
+func readEnv(cfg *Config) map[string]string {
+	result := map[string]string{}
+	for k, v := range cfg.Env {
+		result[k] = replactProjectId(v, cfg.ProjectId)
+	}
+	return result
+}
+
 func readSecret(ctx context.Context, cfg *Config) (map[string]string, error) {
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
@@ -70,7 +88,7 @@ func readSecret(ctx context.Context, cfg *Config) (map[string]string, error) {
 	result := map[string]string{}
 	for k, v := range cfg.Secret {
 		req := &secretmanagerpb.AccessSecretVersionRequest{
-			Name: fmt.Sprintf("projects/%s/secrets/%s/versions/latest", cfg.ProjectId, v),
+			Name: replactProjectId(v, cfg.ProjectId),
 		}
 		resp, err := client.AccessSecretVersion(ctx, req)
 		if err != nil {
@@ -88,4 +106,8 @@ func setEnv(env map[string]string) error {
 		}
 	}
 	return nil
+}
+
+func replactProjectId(s, pid string) string {
+	return strings.Replace(s, "{project_id}", pid, -1)
 }
